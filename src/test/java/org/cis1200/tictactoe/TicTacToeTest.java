df31bb2d10ee9075a96628044ac11c45afa03d47
Maya Kfir <mayakfir@seas.upenn.edu>
2024-12-25 16:24:16 -0500

    changes

---
+++ src/test/java/org/cis1200/tictactoe/TicTacToeTest.java
@@ -0,0 +1,18 @@
+package org.cis1200.tictactoe;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * You can use this file (and others) to test your
+ * implementation.
+ */
+
+public class TicTacToeTest {
+
+    @Test
+    public void test() {
+        assertNotEquals("CIS 1200", "CIS 1600");
+    }
+
+}
